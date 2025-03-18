1872a60996a0b9fab0a68e24e558af4c9708e823
Babacuguara <babacuguara@gmail.com>
2025-03-18 17:02:40 -0300

    feat: create api register 02

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello, World")
@@ -402,13 +402,11 @@ app.put('/produtos/:id', upload.single('foto'), (req, res) => {
   
 
 
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//     console.log(`Servidor rodando na porta ${PORT}`);
-// });
-// const port = Process.env.PORT || 3001
-
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+// app.listen(port, () => {
+//     console.log(`Servidor rodando na porta ${port}`);
+// });
 
